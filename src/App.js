5df59b63a83e478c3ed20034c576c49dd8495313
csomet <csomet@me.com>
2018-11-22 20:54:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import AddNewAppointment from './components/AddNewAppointment';
+import AppointList from './components/AppointList';
+
 
 class App extends Component {
+
+  state = {
+    appointment: []
+  }
+
+  componentDidMount(){
+    const storedAppointments = localStorage.getItem('data');
+    if (storedAppointments){
+      this.setState({
+        appointment : JSON.parse(storedAppointments)
+        //Recover from localStorage when refresh page. Parse from string to JSON (Objetct)
+      })
+    }
+
+  }
+
+  componentDidUpdate(){
+
+    //store in localStorage.
+    localStorage.setItem(
+        'data',
+        JSON.stringify(this.state.appointment)
+        //Convert from JSON (Object) to String
+    )
+  }
+
+  createNewAppointment = newAppoint => {
+    
+    const appmntCopy = [...this.state.appointment, newAppoint]
+
+    this.setState({
+        appointment : appmntCopy
+    });
+     
+  }
+
+  deleteAppointment = id => {
+    
+    //read state 
+    const copyAppointments = [...this.state.appointment];
+
+    //filter to delete
+    const procesedAppointments = copyAppointments.filter(appoint => appoint.id !== id)
+
+    //set the state
+    this.setState({
+      appointment: procesedAppointments
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+          <Header title={'Vet Patients Manager'} />
+     
+      <div className="row">
+          <div className="col-md-6">
+              <AddNewAppointment createNewAppointment={this.createNewAppointment}/>
+          </div>
+          <div className="col-md-6">
+              <AppointList appointments = {this.state.appointment}
+                            deleteAppointment={this.deleteAppointment}/>
+          </div>
       </div>
+     
+      </div>
+     
     );
   }
 }
